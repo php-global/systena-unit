7d19f62db0fee8600fb7d493f021a1896896ec50
DaiDV <daidv@rikkeisoft.com>
2017-12-20 19:07:14 +0700

    master: init phpunit version 6+

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @group failing
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
